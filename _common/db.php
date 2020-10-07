e835ca887af06e19e2d02fdd053c07098af7f1fe
jdwhite <jdwhite@iastate.edu>
2020-10-07 10:09:29 -0500

    Minor formatting changes. Removed debugging code. Avoid fetch() warnings by only fetch()ing if rowCount() > 0.

--- _common/db.php
+++ _common/db.php
@@ -5,24 +5,32 @@
 //
 
 // Perform DB query - returns array of rows.
-// Courtesy of M. Arronax.
+// Courtesy of M. Aronnax.
 
 function simple_query(
-    $sql,
-    $params = array(),
-    $remove_extra_spaces = true
+	$sql,
+	$params = array(),
+	$remove_extra_spaces = true
 ) {
-    $out = array();
-    $pdo = $GLOBALS['pdo'];
-    if ($remove_extra_spaces) {
-        $sql = trim(preg_replace('/\s\s+/', ' ', $sql));
-    }
+	$out = array();
+	$pdo = $GLOBALS['pdo'];
+	if ($remove_extra_spaces) {
+		$sql = trim(preg_replace('/\s\s+/', ' ', $sql));
+	}
 	//print "sql=".$sql."<br>\n";
-    $sth = $pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
-    $sth->execute($params);
-    for ($i=0; $row = $sth->fetch(); $i++) { $out[$i] = $row; }
+	try {
+		$sth = $pdo->prepare($sql, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY));
+		$sth->execute($params);
+		// Check rowCount before fetch()ing to prevent warnings when 
+		// performing queries that return no rows. -jdwhite
+		if ($sth->rowCount() > 0) {
+			for ($i=0; $row = $sth->fetch(); $i++) { $out[$i] = $row; }
+		}
+	} catch (\PDOException $e) {
+		throw new \PDOException($e->getMessage(), (int)$e->getCode());
+	}
 
-    return $out;
+	return $out;
 }
 
 //
@@ -33,13 +41,7 @@ $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 
-//print "db_host=$db_host<BR>";
-//print "db_name=$db_name<BR>";
-//print "db_user=$db_user<BR>";
-//print "db_pass=$db_pass<BR>";
-
 $dsn = "mysql:host=${db_host};dbname=${db_name};charset=utf8mb4";
-//print "dsn=$dsn<br>";
 
 // Get a PDO instance.
 try {
