190b004681ae0c8108d48105731e3fb47333763a
Jason White <jdwhite@menelos.com>
2020-10-26 20:35:01 -0500

    Ensure that DB_ environment variables aren't empty.

--- _common/db.php
+++ _common/db.php
@@ -45,6 +45,11 @@ $db_name = getenv('DB_NAME');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 
+empty($db_host) && die("DB_HOST is empty.");
+empty($db_name) && die("DB_NAME is empty.");
+empty($db_user) && die("DB_USER is empty.");
+empty($db_pass) && die("DB_PASS is empty.");
+
 $dsn = "mysql:host=${db_host};dbname=${db_name};charset=utf8mb4";
 
 // Get a PDO instance.
